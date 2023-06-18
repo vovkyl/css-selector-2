4dcfb3d8debe147f9ea2aa096221d460a89c34ca
Vladimir <Vladimir@Server-iMac.local>
2023-06-19 00:23:21 +0300

    selector 2

--- src/route/index.js
+++ src/route/index.js
@@ -8,11 +8,13 @@ const router = express.Router()
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
+router.get('/index', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+    layout:'index',
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
 
